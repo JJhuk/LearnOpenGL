4ee762b4ce613709fbae95ac1ec2741561e9ae4c
JJhuk <ly0738j@gmail.com>
2021-01-04 02:35:14 +0900

    🔨 shader

---
+++ Shaders/Common/shader.h
@@ -0,0 +1,10 @@
+﻿#pragma once
+#include <glad/glad.h>
+
+class shader
+{
+public:
+	static void print_shader_error(GLuint shader_id);
+	static bool is_success_compile_shader(GLuint shader_id);
+	static GLuint compile_shader(const char* source, GLuint shader_type);
+};
