2d8824526319f1a9d4c9383b35f28198b2bfb101
JJhuk <ly0738j@gmail.com>
2021-01-03 18:17:06 +0900

    🔨 add hellotriangle - exercises 1

---
+++ HelloTriangle/Common/shader.cpp
@@ -0,0 +1,40 @@
+﻿#include "shader.h"
+#include <iostream>
+
+bool shader::is_success_compile_shader(const GLuint shader_id)
+{
+	int success;
+	glGetShaderiv(shader_id, GL_COMPILE_STATUS, &success);
+	return success != 0;
+}
+
+void shader::print_shader_error(const GLuint shader_id)
+{
+	char info_log[512];
+
+	if (is_success_compile_shader(shader_id)) return;
+
+	glGetShaderInfoLog(shader_id, 512, nullptr, info_log);
+	std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << info_log << std::endl;
+}
+
+GLuint shader::compile_shader(const char* source, const GLuint shader_type)
+{
+	if (!(shader_type == GL_FRAGMENT_SHADER || shader_type == GL_VERTEX_SHADER))
+	{
+		std::cout << "shader_type supported only GL_FRAGMENT_SHADER and GL_VERTEX_SHADER";
+		return NULL;
+	}
+
+	const auto shader = glCreateShader(shader_type);
+	glShaderSource(shader, 1, &source, nullptr);
+	glCompileShader(shader);
+
+	if(!is_success_compile_shader(shader))
+	{
+		print_shader_error(shader);
+		return -1;
+	}
+
+	return shader;
+}
---
+++ HelloTriangle/Exercises1/HelloTriangle - Exercises1.cpp
@@ -0,0 +1,119 @@
+﻿#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include "../Common/shader.h"
+
+#define MAJOR_VERSION 3
+#define MINOR_VERSION 3
+#define WIDTH 800
+#define HEIGHT 600
+
+float vertices[] = {
+	-1.0f, -0.5f, 0.0f, // 왼쪽 삼각형 왼쪽
+	-0.5f, 0.5f, 0.0f, // 왼쪽 삼각형 위
+	-0.0f, -0.5f, 0.0f, // 왼쪽 삼각형 오른쪽
+	
+	-0.0f, -0.5f, 0.0f, // 오른쪽 삼각형 왼쪽
+	0.5f, 0.5f, 0.0f, // 오른쪽 삼각형 위
+	1.0f, -0.5f, 0.0f, // 오른쪽 삼각형 오른쪽
+};
+
+const char * vertex_shader_source = R"(
+	#version 330 core
+	layout (location = 0) in vec3 aPos;
+	void main()
+	{
+		gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+	})";
+
+const char * fragment_shader_source = R"(
+	#version 330 core
+	out vec4 FragColor;
+
+	void main()
+	{
+		FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+	})";
+
+void frame_buffer_size_callback(GLFWwindow* window, const int width, const int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+int main()
+{
+	// WINDOW CREATE AND INIT
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, MAJOR_VERSION);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, MINOR_VERSION);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	const auto window = glfwCreateWindow(WIDTH, HEIGHT, "Exercise1",
+	                                     nullptr, nullptr);
+
+	if(!window)
+	{
+		std::cout << "Faild Create Window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	
+	glfwMakeContextCurrent(window);
+	
+	if(!gladLoadGLLoader(reinterpret_cast<GLADloadproc>(glfwGetProcAddress)))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+
+	//compile and linking shader
+	auto vertex_shader = shader::compile_shader(vertex_shader_source, GL_VERTEX_SHADER);
+	auto fragment_shader = shader::compile_shader(fragment_shader_source, GL_FRAGMENT_SHADER);
+	auto program = glCreateProgram();
+
+	glAttachShader(program, vertex_shader);
+	glAttachShader(program, fragment_shader);
+	glLinkProgram(program);
+
+	glDeleteShader(vertex_shader);
+	glDeleteShader(fragment_shader);
+	
+
+	// BUFFER,ARRAY OBJECT
+	unsigned int vao, vbo;
+	glGenVertexArrays(1, &vao);
+	glGenBuffers(1, &vbo);
+
+	glBindVertexArray(vao);
+	
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof vertices, vertices, GL_STATIC_DRAW);
+
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
+	glEnableVertexAttribArray(0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, NULL);
+	glBindVertexArray(NULL);
+
+	glfwSetFramebufferSizeCallback(window, frame_buffer_size_callback);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glUseProgram(program);
+		glBindVertexArray(vao);
+		glDrawArrays(GL_TRIANGLES, 0, 6); //삼각형 6개 버텍스 그리기
+		
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glDeleteVertexArrays(1, &vao);
+	glDeleteBuffers(1, &vbo);
+	glDeleteProgram(program);
+	glfwTerminate();
+	return 0;
+}
